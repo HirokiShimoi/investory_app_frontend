bc507ad3f84e17dde1cdd68999dbde1b1747c6ca
Hiroki Shimoie <apelox1gmail.com>
2023-09-01 20:37:56 +0900

    remote

--- src/pages/toppage.js
+++ src/pages/toppage.js
@@ -1,62 +1,15 @@
-import React from 'react';
-import Header from './header';  // 作成済みのヘッダーコンポーネント
-import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
+import React from 'react'
+import Header from './header'
 
-const styles = {
-    card: {
-      width: '400px',
-      height: '400px',
-    },
-  };
+function toppage() {
+    return (
+        <>
+            <Header />
+            <div className=''>
 
-function TopPage() {
-  return (
-    <div>
-      <Header />
-      <Container>
-        <Grid container spacing={3}>
-          {/* 上段のカード 3枚 */}
-          <Grid item xs={4}>
-            <Card style={styles.card}>
-              <CardContent>
-                <Typography variant="h5">弥生在庫CSV整形</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5">CSVデータをインポート</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5">発注点を下回る在庫</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          {/* 下段のカード 2枚 */}
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5">商品データの編集</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5">発注コメント</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+            </div>
+        </>
+    )
 }
 
-export default TopPage;
+export default toppage

b495e8b6d220b44b7009df00a9468424eebc8252
HirokiShimoi <you@example.com>
2023-09-01 18:52:01 +0900

    管理画面作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './pages/login';
+import TopPage from './pages/toppage';
 import { AuthProvider } from './context/authcontext';
 import Header from './pages/header';
 import { BrowserRouter as Router, Routes ,Route, useNavigate } from "react-router-dom";
@@ -12,6 +13,7 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element= {<Login/>}/>
+                    <Route path="/admin/" element= {<TopPage/>}/>
                 </Routes>
             </Router>
         </AuthProvider>
---
+++ src/pages/toppage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Header from './header';  // 作成済みのヘッダーコンポーネント
+import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
+
+const styles = {
+    card: {
+      width: '400px',
+      height: '400px',
+    },
+  };
+
+function TopPage() {
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Grid container spacing={3}>
+          {/* 上段のカード 3枚 */}
+          <Grid item xs={4}>
+            <Card style={styles.card}>
+              <CardContent>
+                <Typography variant="h5">弥生在庫CSV整形</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5">CSVデータをインポート</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5">発注点を下回る在庫</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          {/* 下段のカード 2枚 */}
+          <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5">商品データの編集</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5">発注コメント</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default TopPage;

87ad4a45a442384039c7c51aa9fc357adc801ab1
HirokiShimoi <you@example.com>
2023-09-09 19:33:22 +0900

    checkeditemコンポーネント作成

--- src/pages/checkeditem.tsx
+++ src/pages/checkeditem.tsx
@@ -2,44 +2,75 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { DataGrid, GridColDef, GridRowsProp, GridValueGetterParams } from '@mui/x-data-grid';
 import {Tabs, Tab} from '@mui/material';
+import Button from '@mui/material/Button';
 
+type Product = {
+    id: number;
+    product_code: string;
+    product_name: string;
+    inventory: number;
+    category: string;
+    orderpoint: number;
+  };
+  
+  type Comment = {
+    product: Product;
+  };
 
 function CheckedItem() {
-    const [value, setValue] = useState(0);
-    const [data,setData] = useState<GridRowsProp>([]);
+    const [selectedData, setSelectedData] = useState<GridRowsProp>([]);
+    const [comments, setComments] = useState<Comment[]>([]);
+
+    const mergeProductAndComments = (products: Product[], comments: Comment[]) => {
+        return products.map(product => {
+          const relatedComment = comments.find(comment => comment.product.product_code === product.product_code);
+          return {
+            ...product,
+            comment: relatedComment ? 'コメントあり' : 'コメントなし',
+          };
+        });
+      };
+
 
     useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/api/selecteditem/`)
-        .then(response => {
-            const transformedData = response.data.results.map((item) => ({
-                id: item.id, 
-                product_code: item.product.product_code,
-                product_name: item.product.name,
-                inventory: item.current_stock,
-                category: item.product.category,
-                orderpoint:item.reorder_point,
-            }));
-            setData(transformedData);
-            console.log("Transformed data: ", transformedData);
-        })
-        .catch(error => {
-            console.log('Error fetching selected items:', error);
-        });
-    }, []
+        const fetchSelectedData = axios.get('http://127.0.0.1:8000/api/selecteditem/');
+        const fetchComments = axios.get('http://127.0.0.1:8000/api/comment/');
     
-    );
+        Promise.all([fetchSelectedData, fetchComments])
+          .then(([selectedDataRes, commentsRes]) => {
+            const transformedData: Product[] = selectedDataRes.data.map((item: Product) => {
+              return {
+                id: item.id,
+                product_code: item.product_code,
+                product_name: item.product_name,
+                inventory: item.inventory,
+                category: item.category,
+                orderpoint: item.orderpoint,
+              };
+            });
+            setComments(commentsRes.data);
+            const mergedData = mergeProductAndComments(transformedData, commentsRes.data);
+            setSelectedData(mergedData);
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+            // Here you can handle the error more gracefully
+          });
+      }, []);
+
 
     const columns : GridColDef[] = [
         {field: 'product_code', headerName: 'インストア', flex: 1 },
         {field: 'product_name', headerName: '商品名', flex: 2 },
         {field: 'inventory', headerName: '在庫', flex: 1 },
         {field: 'orderpoint', headerName: '発注点', flex:1 },
+        { field: 'comment', headerName: 'コメント', flex: 1 },
     ]
 
     return (
         <div style={{ height: '100vh', width: '100%' }}>
             <DataGrid 
-                 rows={data}
+                 rows={selectedData}
                  columns={columns}
                  pageSizeOptions={[5, 10]}
                  checkboxSelection
